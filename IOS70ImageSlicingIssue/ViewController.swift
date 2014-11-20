36c486d323882e34aa50cd0c6fe7ef9748cd2dd5
laiso <laiso@lai.so>
2014-11-20 13:19:30 +0900

    Initial commit

---
+++ IOS70ImageSlicingIssue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS70ImageSlicingIssue
+//
+//
+//  Copyright (c) 2014 laiso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ iOS 7.0 Image Slicing Issue
 ==============================
 
 The app to crash on iOS 7.0 when set the invalid size of Image Slicing in xcasset.
+
+http://qiita.com/laiso/items/393d98de97f41d46aab2
